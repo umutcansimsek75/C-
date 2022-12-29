878ffc4df4309ad5feb0ffccc38b0f2f1b081c3f
umutcansimsek75 <119808448+umutcansimsek75@users.noreply.github.com>
2022-12-29 21:32:15 +0300

    SayısalLoto

---
+++ sayisalloto/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace sayisalloto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int[] sayilar=new int[6];
+int[] kasaSayilar=new int[6];
+int tahmin= 0;
+
+Console.WriteLine("lütfen 6 adet sayı giriniz");
+Random rnd =new Random();
+
+for (int i = 0; i < 6; i++)
+{
+    kasaSayilar[i]= rnd.Next(1,60);
+    Console.WriteLine("{0}. sayiyi giriniz :",(i+1));
+    sayilar[i]=int.Parse(Console.ReadLine()!);
+}
+
+foreach (var sayi in sayilar)
+{
+    foreach (var kasaSayi in kasaSayilar)
+    {
+        tahmin = sayi == kasaSayi ? tahmin++ : tahmin;
+    } 
+}
+Console.WriteLine("{0} adet doğru tahminde bulundunuz", tahmin);
+Console.WriteLine("Random sayilar:");
+for (int i = 0; i < kasaSayilar.Length; i++)
+{
+ Console.WriteLine("{0}",kasaSayilar[i]);   
+}
+}}
+}
