221add9cb5d414deb2a5c57245cc752c057e4371
umutcansimsek75 <119808448+umutcansimsek75@users.noreply.github.com>
2023-01-05 21:26:16 +0300

    operatörler

---
+++ operatorler/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 8, b = 7;
+Console.WriteLine("a sayısı: "+ a + ", b sayısı"+ b);
+
+Console.WriteLine("a sayısı: {0}, b sayısı {1} ", a, b);
+
+Console.WriteLine("++a => {0}", (++a));
+Console.WriteLine("a+=2 => {0}", (a+=2));
+Console.WriteLine("a==b => {0}", (a==b));
+Console.WriteLine("a>b  => {0}", (a>b));
+Console.WriteLine("!(a==b) => {0}", !(a==b));
+
+Console.WriteLine("!(true&&true) => {0}", !(true&&true));
+Console.WriteLine("!(true&&false) => {0}", !(true&&false));
+Console.WriteLine("!(true||false) => {0}", !(true||false));
+Console.WriteLine("!(true {0}", !!true);
